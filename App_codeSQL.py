78a98e1c25713d0ae29e071f29f07d992eba2f09
ismo2412 <mfofana2412@gmail.com>
2026-04-17 22:54:55 +0200

    add theme sidebar

--- App_codeSQL.py
+++ App_codeSQL.py
@@ -8,19 +8,25 @@ df = pd.DataFrame(data)
 conn = duckdb.connect()
 conn.register("data_source", df)
 
-# Selectbox avec des requêtes exemples
-queries = {
-    "Choisir une requête...": "",
-    "Tout afficher": "SELECT * FROM data_source",
-    "Somme de a": "SELECT SUM(a) AS somme_a FROM data_source",
-    "Moyenne de b": "SELECT AVG(b) AS moyenne_b FROM data_source",
-    "Filtrer a > 1": "SELECT * FROM data_source WHERE a > 1"
-}
+# Sidebar avec "with"
+with st.sidebar:
+    st.title("📌 Menu")
 
-selected_query = st.selectbox("📌 Requêtes exemples", list(queries.keys()))
+    queries = {
+        "Choisir une requête...": "",
+        "Tout afficher": "SELECT * FROM data_source",
+        "Somme de a": "SELECT SUM(a) AS somme_a FROM data_source",
+        "Moyenne de b": "SELECT AVG(b) AS moyenne_b FROM data_source",
+        "Filtrer a > 1": "SELECT * FROM data_source WHERE a > 1"
+    }
 
-# Pré-remplir le text_area avec la requête choisie
+    selected_query = st.selectbox("Requêtes exemples", list(queries.keys()))
+
+# Valeur par défaut
 default_query = queries[selected_query]
+
+# Zone principale
+st.title("🧠 Explorateur SQL avec DuckDB")
 
 query = st.text_area("Ton code SQL", value=default_query)
 
